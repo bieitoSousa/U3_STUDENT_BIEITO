78572c0e2ef3be09b79c0bafd24bc590697f70d2
Bieito Sousa <bieito.studies@gmail.com>
2021-02-12 22:07:38 +0100

    update Block1

--- app/src/main/java/com/bieito_sousa/u3_student_bieito/MainActivity.java
+++ app/src/main/java/com/bieito_sousa/u3_student_bieito/MainActivity.java
@@ -1,24 +1,37 @@
 package com.bieito_sousa.u3_student_bieito;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    final TextView textView = findViewById(R.id.text_view_id);
+    final CheckBox checkBox = findViewById(R.id.checkbox_id);
+    final EditText editText = findViewById(R.id.plain_text_input);
+    final RadioButton radioButton_red = findViewById(R.id.radio_red);
+    final RadioButton radioButton_blue = findViewById(R.id.radio_blue);
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +40,35 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(checkBox.isChecked()){
+                    textView.setText("");
+                }else {
+                    textView.setText(editText.getText());
+                }
+            }
+        });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_red:
+                if (checked)
+                    this.radioButton_blue.setChecked(false);
+                    this.textView.setTextColor(ContextCompat.getColor(this,R.color.red));
+                    break;
+            case R.id.radio_blue:
+                if (checked)
+                    this.radioButton_red.setChecked(false);
+                    this.textView.setTextColor(ContextCompat.getColor(this,R.color.blue));
+                    break;
+        }
     }
 
     @Override
